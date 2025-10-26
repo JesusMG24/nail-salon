050fc44ef62a4d2aa3bc74fd5b6a9a002ac347fb
Jesus Mendoza <albertomendozax9800@gmail.com>
2025-10-25 22:36:19 -0600

    fixed frontend url problem

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["https://frontend-production-46e3.up.railway.app"],
 };
 
 export default nextConfig;
