0c2dba5f897324763de658bf06be789b50097df0
Jesus Mendoza <albertomendozax9800@gmail.com>
2025-10-28 03:32:59 -0600

    preparing production env

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import app from "./app";
 import config from "./config/config";
 
-app.listen(config.port, () => {
-  console.log(`Server running on port ${config.port}`);
+const port = process.env.PORT || config.port || 3000;
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
